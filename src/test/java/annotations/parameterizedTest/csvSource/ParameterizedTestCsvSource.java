9afdd635769266ae1362297b6373dc4d890a1873
v-giorgio7 <vgiorgio@ciandt.com>
2022-06-24 10:32:35 -0300

    bugfix: changed file name

--- src/test/java/annotations/parameterizedTest/csvSource/ParameterizedTestCsvSource.java
+++ src/test/java/annotations/parameterizedTest/csvSource/ParameterizedTestCsvSource.java
@@ -1,11 +1,10 @@
-package test.java.annotations.parametrizedTest.csvSource;
+package test.java.annotations.parameterizedTest.csvSource;
 
 import main.java.livraria.model.Livro;
 import main.java.livraria.service.LivroService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
